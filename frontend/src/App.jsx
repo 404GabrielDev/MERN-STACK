9c5ae6a1d56b3cbeb872157cc0a9dc19f27a2134
404GabrielDev <joaogabriell.ssm@gmail.com>
2025-01-15 22:22:27 -0300

    correção de inputs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,16 @@ import Header from "./components/navbar/header"
 import { Routes, Route} from "react-router-dom"
 import Form from "./components/form/Form"
 import ForgetPassword from "./components/ForgetPassowrd/ForgetPassoword"
+import { useState } from "react"
 
 function App() {
-  
+  const[username, setUsername] = useState("")
 
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/form" element={<Form />} />
+        <Route path="/form" element={<Form setUsername={setUsername}/>} />
         <Route path="/forgetPassword" element={<ForgetPassword />} />
       </Routes>
     </>
